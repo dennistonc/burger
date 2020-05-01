261f65cc87480813c9dbad6418185d20a1b8a218
dennistonc <dennistonc@gmail.com>
2020-05-01 00:56:02 -0500

    file and folder setup, still need handlebars setup

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- DROP TABLE IF EXISTS burgers_db;
+
+CREATE TABLE burgers (
+	id INTEGER AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(50),
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whatachick'n", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Patty Melt", false);
